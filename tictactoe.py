681a1d3d802effcdf6a91c3b832576199a2a68e1
autobotJimi <jimi.spier@gmail.com>
2022-09-28 21:14:12 -0700

    working on logic

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,19 @@
 rows, cols = (3, 3)
 board = [['']*cols]*rows
 
+def board_layout(*args):
+    board_dict = {"rows":{1:("_","_","_"),
+                          2:("_","_","_"),
+                          3:("_","_","_")}}
+
+    #--First Row
+    print(board_dict["rows"][1][0] + "|" + board_dict["rows"][1][1] + "|" + board_dict["rows"][1][2])
+    #--Second Row
+    print(board_dict["rows"][2][0] + "|" + board_dict["rows"][2][1] + "|" + board_dict["rows"][2][2])
+    #--Third Row
+    print(board_dict["rows"][3][0] + "|" + board_dict["rows"][3][1] + "|" + board_dict["rows"][3][2])
+
+
 def full_board(board):
     squares_filled = 0
     for square in board:
@@ -12,6 +25,6 @@ def full_board(board):
         return True
     else:
         return False
-#
+
 print(full_board(board))
 print(board)
