3e25e218ad788830da7b781fb160f0285f4e9dc5
hieul98 <hieule1998@yahoo.com>
2022-09-29 19:35:29 -0700

    added winning combination method

--- tictactoe.py
+++ tictactoe.py
@@ -32,3 +32,56 @@ winning_combinations = {[row = 1: cols = 1, row = 1: cols = 2, row = 1, cols = 3
                         [row = 1 : col = 1, row = 2 : cols = 2, row = 3 : col = 3],
                         [row = 3 : col = 1, row = 2 : cols = 2, row = 1 : col = 3]
                         }
+
+row = {
+    'top': 0, 
+    'middle': 1, 
+    'bottom': 2
+    }
+col = {
+    'left': 0, 
+    'middle': 1, 
+    'right': 2}
+​
+def player_move():
+    position = input("What's your next move? ")
+    position = position.split(' ')
+    x = row[position[0]]
+    y = col[position[1]]
+    return x, y
+​
+print(player_move())
+
+
+def winning_combination(board, symbol_1, symbol_2):
+    winner = True
+    #rows
+    for row in range (0, 3):
+        if (board[row][0] == board[row][1] == board[row][2] == symbol_1):
+            winner = False
+            print("Player " + symbol_1 + ", you won!")
+        elif (board[row][0] == board[row][1] == board[row][2] == symbol_2):
+            winner = False
+            print("Player " + symbol_2 + ", you won!")   
+    #columns
+    for col in range (0, 3):
+        if (board[0][col] == board[1][col] == board[2][col] == symbol_1):
+            winner = False
+            print("Player " + symbol_1 + ", you won!")
+        elif (board[0][col] == board[1][col] == board[2][col] == symbol_2):
+            winner = False
+            print("Player " + symbol_2 + ", you won!")
+    # diagnoals
+    if board[0][0] == board[1][1] == board[2][2] == symbol_1:
+        winner = False 
+        print("Player " + symbol_1 + ", you won!")
+    elif board[0][0] == board[1][1] == board[2][2] == symbol_2:
+        winner = False
+        print("Player " + symbol_2 + ", you won!")
+    elif board[0][2] == board[1][1] == board[2][0] == symbol_1:
+        winner = False
+        print("Player " + symbol_1 + ", you won!")
+    elif board[0][2] == board[1][1] == board[2][0] == symbol_2:
+        winner = False
+        print("Player " + symbol_2 + ", you won!")
+    return winner
